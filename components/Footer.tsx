e82d54ca24d03d2a7cce7d799a296ebf26548d04
Kadir <116947551+serpikoz@users.noreply.github.com>
2023-12-25 15:36:54 +0300

    continue-adding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,38 @@
+import { FOOTER_LINKS } from "@/constanst";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}></FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+    </div>
+  );
 };
 
 export default Footer;
